22eb3be6e93f046a9df99bd8ec01f00a46242fef
Gautham Arakala <87577583+Gautam0018@users.noreply.github.com>
2023-04-04 16:25:18 +0530

    Create Letter repetition.c

---
+++ Letter repetition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("G");
+    for(int i=0; i<n; i++){
+        printf("o");
+    }
+    printf("d");
+    return 0;
+}
